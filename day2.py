b06cc6fae7322717ec4912118b27147ca6dcc6fb
PREETHI  KM <preethikmpreethikm1@gmail.com>
2025-03-17 21:57:02 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,49 @@
+#variables in python
+
+#1.User details in a social media app
+username = "john_doe"
+age = 25
+followers = 1500
+print("User:", username)
+print("Age:", age)
+print("Followers:", followers)
+#Output:
+User: john_doe  
+Age: 25  
+Followers: 1500
+
+#2.Bank balance system
+balance = 5000  # Initial balance
+deposit = 2000  # Amount deposited
+balance += deposit  # Adding deposit to balance
+
+print("Updated Balance:", balance)
+
+#Output:
+Updated Balance: 7000
+
+#3. Temperature Conversion (Celsius to Fahrenheit)
+
+# Converting temperature from Celsius to Fahrenheit
+celsius = 30  
+fahrenheit = (celsius * 9/5) + 32  
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
+#Output:
+Temperature in Fahrenheit: 86.0
+
+#4. Grocery Store Billing System
+# Grocery store billing
+apple_price = 20  
+banana_price = 10  
+apple_quantity = 2  
+banana_quantity = 5  
+
+total_cost = (apple_price * apple_quantity) + (banana_price * banana_quantity)
+
+print("Total Grocery Cost:", total_cost)
+
+#Output:
+Total Grocery Cost: 70
+
